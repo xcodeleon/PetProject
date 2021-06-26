ce7903b38c7ef6d459e8f36098459be73277a45a
Eugene <xcodeleon@gmail.com>
2021-06-26 16:12:28 +0300

    Renaming files in a folder

---
+++ src/MainRenameFiles.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+
+
+import static java.lang.Math.random;
+
+public class MainRenameFiles {
+
+    public static void main(String[] args) throws IOException {
+
+        File[] filesList;
+        File filesPath = new java.io.File("D:\\picture\\oboi"); // создаем объект на папку с файлами
+
+        filesList = filesPath.listFiles(); // записываем файлы из папки в массив объектов типа File
+
+        for (int i = 0; i < filesList.length; i++) {
+            String buf = filesList[i].getName(); // читаем текущее имя файла
+            if (buf.endsWith(".jpg")) {
+                buf = buf.concat("1234" + ".jpg");
+                filesList[i].renameTo(new File("D:\\wallpapers\\" + buf));
+            }
+        }
+    }
+}
