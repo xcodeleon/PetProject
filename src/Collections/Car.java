8104bb88373385efbde46eb1d7c12f087ceea11a
Eugene <xcodeleon@gmail.com>
2021-07-30 21:28:26 +0300

    Реализация HashSet

---
+++ src/Collections/Car.java
@@ -0,0 +1,40 @@
+package Collections;
+
+import java.util.Objects;
+
+public class Car {
+    private String brand;
+    private int number;
+
+
+    public Car(String brand, int number) {
+        this.brand = brand;
+        this.number = number;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return number == car.number &&
+                Objects.equals(brand, car.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, number);
+    }
+}
